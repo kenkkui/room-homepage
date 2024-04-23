3b35877fc37b84d79d614534c2ab661b02deec87
kenkkui <thifirstime@gmail.com>
2024-04-23 18:34:56 +0800

    add boiling plate for design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./index.css";
+import iconAngleLeft from "./images/icon-angle-left.svg";
+import iconAngleRight from "./images/icon-angle-right.svg";
 
 function App() {
-  return <></>;
+  return (
+    <main id="app">
+      <section id="upper-section">
+        <section className="main-image-grid"></section>
+        <section className="main-context-grid"></section>
+
+        <section className="btn-container">
+          <button className="prev-btn">
+            <img src={iconAngleLeft} alt="previous button" />
+          </button>
+
+          <button className="next-btn">
+            <img src={iconAngleRight} alt="next button" />
+          </button>
+        </section>
+      </section>
+
+      <section id="lower-section">
+        <section className="about-our-furniture"></section>
+      </section>
+    </main>
+  );
 }
 
 export default App;
